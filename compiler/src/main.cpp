dfdd73ae86752c3952638681cc7dedd9cb5bbe09
Tim <76897954+timsexperiments@users.noreply.github.com>
2025-09-04 17:50:30 -0500

    Update compiler/src/main.cpp

--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv) {
     if (argc > 1 && std::string_view{argv[1]} == "--version") {
-        std::cout << "tofu 0.0.1" << std::endl;
+        std::cout << "tofu 0.0.0" << std::endl;
         return 0;
     }
     std::cout << "Tofu compiler (work in progress)" << std::endl;
